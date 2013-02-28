d4d86bef9ebbe4766c3f36d3ee9ef80ad48bbd8c
Romans Malinovskis <r@agiletech.ie>
2013-02-28 17:39:12 +0000

    initial commit

---
+++ lib/Controller/Validator.php
@@ -0,0 +1,83 @@
+<?php
+namespace romaninsh\validation;
+
+/**
+ * Implementation of hook-based validation / normalization controller
+ *
+ * Add this controller inside model or form, then configure.
+ */
+
+class Controller_Validator extends \AbstractController {
+    public $_conf;
+    public $configure=null;
+
+    function init(){
+        parent::init();
+        // you can specify configuration thourgh add's
+        // second parameter: add('..',array('configure'=>..));
+        // or chain.
+        if($this->configure)$this->configure($this->configure);
+    }
+    function configure($callback,$config){
+        if($callback===true){
+            $this->parse($this,$config);
+        }else{
+            $this->owner->addHook($callback,array($this,'parse'),array($config));
+        }
+        return $this;
+    }
+    function parse($m,$config)
+    {
+        foreach ($config as $field => $rules) {
+            if (is_string($rules)) {
+                $rules=explode(',', $rules);
+            }
+
+            $v_orig=$v=$this->owner[$field];
+
+
+            foreach ($rules as $rule) {
+                $args=explode(':',$rule);
+                $rule=array_shift($args);
+
+                if(substr($rule,-1)=='!'){
+                    $rule=substr($rule,0,-1);
+                    $throw=true;
+                }else $throw=false;
+                $e=false;
+
+                switch ($rule) {
+                    case 'int':
+                        $e='Should be integer';
+                        $v=(int)$v;
+                        break;
+                    case 'max':
+                        $e='Must be no bigger than %s';
+                        if($v>$arg[0])$v=$arg[0];
+                        break;
+                    case 'min':
+                        $e='Must be at least %s';
+                        if($v<$arg[0])$v=$arg[0];
+                        break;
+                    case 'between':
+                        $e='Must be between %s and %s';
+                        if($v<$arg[0])$v=$arg[0];
+                        if($v>$arg[1])$v=$arg[1];
+                        break;
+                    case 'email':
+                        // TODO: clean up email
+                        break;
+                    default:
+
+                }
+                if($v!==$v_orig && $throw){
+                    throw $this->exception($e?:'Validaiton error','ValidityCheck')
+                        ->setField($field)
+                        ;
+                }
+            }
+
+            $v_orig=$v=$this->owner[$field];
+        }
+    }
+}
